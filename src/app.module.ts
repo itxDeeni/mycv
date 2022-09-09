2ff91107094a8af99e9cbac9427c3217fb79f368
Zahradeen Muazu <Zahradeenmuazu@yahoo.com>
2022-09-09 12:02:31 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+
+@Module({
+  imports: [TypeOrmModule, UsersModule, ReportsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
